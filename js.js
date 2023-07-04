f1d9f569bf0cd7ddd1007f0f550057a41b90b9de
BRYAN_CASTILLO <bryantangamashi@gmail.com>
2023-07-04 13:07:37 -0500

    agregue un boton y un ejercicio de multiplicacion

--- js.js
+++ js.js
@@ -22,3 +22,14 @@ function calcularResiduo() {
     var residuo = dividendo % divisor;
     alert("El residuo de la división es: " + residuo);
 }
+
+function multiplicacion() {
+
+    var x1 = parseInt(document.getElementById("x1").value);
+    var y1 = parseInt(document.getElementById("y1").value);
+    var x2 = parseInt(document.getElementById("x2").value);
+    var y2 = parseInt(document.getElementById("y2").value);
+    
+    r=x1*x2*y1*y2
+    alert("La multiplicacion es: "+ r);
+    }
